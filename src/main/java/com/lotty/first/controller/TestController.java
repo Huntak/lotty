bdd42f3fd1ddcdfe9e53091f067598461ccdbd2c
Huntak <oht5080@gmail.com>
2020-03-05 15:13:11 +0900

    1. 지난회차 로또번호 조회 후 csv 파일로 저장, 읽기 추가 2. 지난회차 중 가장 많이, 적게 나온 상위, 하위 10개 번호 추출 기능 추가

--- src/main/java/com/lotty/first/controller/TestController.java
+++ src/main/java/com/lotty/first/controller/TestController.java
@@ -1,8 +1,19 @@
 package com.lotty.first.controller;
 
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.Reader;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Iterator;
 import java.util.List;
+import java.util.Map.Entry;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.stream.Collectors;
 
+import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
@@ -15,28 +26,62 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
-import com.lotty.first.vo.LottoVO;
-
 @RestController
 public class TestController {
+	JSONParser parser = new JSONParser();
+	String today = new SimpleDateFormat("yyyyMMdd").format(Calendar.getInstance().getTime());
+	String apiUrl = "https://www.nlotto.co.kr/common.do?method=getLottoNumber&drwNo=";
+	String src = "src/main/resources/static/";
+	String jsonFileName = "prevLottoList_";
+
+	@GetMapping("/make")
+	public String make() {
+		// 지난회차 로또번호 조회 후 csv 파일로 저장
+		// 성공 : SUCCESS, 실패 : FAIL
+		return makePrevLottoData();
+	}
 
 	@GetMapping("/test")
-	public ArrayList<LottoVO> test() {
+	public JSONArray test() {
 		// 지난회차 로또번호 조회
-		ArrayList<LottoVO> prevLottoList = getPrevLottoData();
+		JSONArray prevLottoList = readPrevLottoData();
 
-		// TODO : 로또번호 분석 analysisPrevLotto()
+		// 로또번호 카운트
+		ConcurrentHashMap<Integer, Integer> lottoNumCount = countPrevLotto(prevLottoList);
 
-		// TODO : 로또번호 추천 recommandLottoNum()
+		// 상위 10개 최대값, 인덱스 구하기
+		List<Entry<Integer, Integer>> topNLottoNum = lottoNumCount.entrySet()
+																	.stream()
+																	.sorted((e1, e2) -> Integer.compare(e2.getValue(), e1.getValue())) // Sort by value.
+																	.limit(10)
+																	.collect(Collectors.toList());
+
+		// 하위 10개 최대값, 인덱스 구하기
+		List<Entry<Integer, Integer>> bottomNLottoNum = lottoNumCount.entrySet()
+																		.stream()
+																		.sorted((e1, e2) -> Integer.compare(e1.getValue(), e2.getValue())) // Sort by value.
+																		.limit(10)
+																		.collect(Collectors.toList());
+
+		// TODO : 삭제 (값 확인용)
+		topNLottoNum.forEach(l -> {
+			System.out.print(l.getKey() + "번:" + l.getValue() + "회, ");
+		});
+		System.out.println("");
+		bottomNLottoNum.forEach(l -> {
+			System.out.print(l.getKey() + "번:" + l.getValue() + "회, ");
+		});
+		System.out.println("");
+
+		// 로또번호 추천
+		recommandLottoNum(topNLottoNum);
 
 		return prevLottoList;
 	}
 
-	// TODO : 1회차만 가져오지말고 전체 회차 가져와서 ArrayList에 담기
-	private ArrayList<LottoVO> getPrevLottoData() {
-		// 지난회차 로또 정보 담을 리스트
-		ArrayList<LottoVO> prevLottoList = new ArrayList<LottoVO>();
 
+	@SuppressWarnings("unchecked")
+	private String makePrevLottoData() {
 		// RestTemplate 에 MessageConverter 세팅
 		List<HttpMessageConverter<?>> converters = new ArrayList<HttpMessageConverter<?>>();
 		converters.add(new FormHttpMessageConverter());
@@ -49,58 +94,153 @@ public class TestController {
 		MultiValueMap<String, String> map = new LinkedMultiValueMap<String, String>();
 		map.add("str", "thisistest");
 
-		JSONParser parser = new JSONParser();
-		JSONObject obj = null;
+		JSONArray prevLottoList = new JSONArray();
+		JSONObject lotto = new JSONObject();
 		int index = 1;
 
 		// 리턴값이 success가 아닌 값이 나올때까지 계속
 		// TODO : 무한 루프 조심
-		while(true){
+		while (true) {
 			// REST API 호출
-			String resultText = restTemplate.getForObject("https://www.nlotto.co.kr/common.do?method=getLottoNumber&drwNo=" + index, String.class);
+			String resultText = restTemplate.getForObject(apiUrl + index, String.class);
 
-			// 피싱 실패 시 예외처리
+			// 파싱 실패 시 예외처리
 			try {
-				obj = (JSONObject) parser.parse(resultText);
+				lotto = (JSONObject) parser.parse(resultText);
 			} catch (ParseException e) {
-				System.out.println("변환에 실패");
+				System.out.println("JSON 파싱 실패");
 				e.printStackTrace();
+				return "FAIL";
 			}
 
-			System.out.println(obj);
+			// TODO : 삭제 (값 확인용)
+			System.out.println(lotto);
 
-			if(!((String) obj.get("returnValue")).equals("success")) {
+			// 리턴값이 success가 아닌 값이 나올때까지 계속
+			if (!((String) lotto.get("returnValue")).equals("success")) {
 				break;
 			}
 
-			// 회차별로 객체에 담기
-			LottoVO lotto = new LottoVO();
-			lotto.setReturnValue((String) obj.get("returnValue"));
-			lotto.setDrwNo((Long) obj.get("drwNo"));
-			lotto.setDrwtNo1((Long) obj.get("drwtNo1"));
-			lotto.setDrwtNo2((Long) obj.get("drwtNo2"));
-			lotto.setDrwtNo3((Long) obj.get("drwtNo3"));
-			lotto.setDrwtNo4((Long) obj.get("drwtNo4"));
-			lotto.setDrwtNo5((Long) obj.get("drwtNo5"));
-			lotto.setDrwtNo6((Long) obj.get("drwtNo6"));
-			lotto.setBnusNo((Long) obj.get("bnusNo"));
-			lotto.setFirstWinamnt((Long) obj.get("firstWinamnt"));
-			lotto.setFirstPrzwnerCo((Long) obj.get("firstPrzwnerCo"));
-			lotto.setFirstAccumamnt((Long) obj.get("firstAccumamnt"));
-			lotto.setTotSellamnt((Long) obj.get("totSellamnt"));
-			lotto.setDrwNoDate((String) obj.get("drwNoDate"));
+			// json array에 저장
 			prevLottoList.add(lotto);
 
 			// 다음 회차
 			index++;
 		}
 
+		// csv 파일로 저장
+		// try (FileWriter prevLottoListFile = new FileWriter(src + jsonFileName + today + ".json")) {
+		try (FileWriter prevLottoListFile = new FileWriter(src + jsonFileName + ".json")) {
+			prevLottoListFile.write(prevLottoList.toJSONString());
+		} catch (IOException e) {
+			System.out.println("CSV 파일 변환 실패");
+			e.printStackTrace();
+			return "FAIL";
+		}
+
+		return "SUCCESS";
+	}
+
+	private JSONArray readPrevLottoData() {
+		JSONArray prevLottoList = new JSONArray();
+
+		// 이름이 'prevLottoList_yyyymmdd'인(오늘날짜) csv 파일 읽기
+		// try (Reader prevLottoListFile = new FileReader(src + jsonFileName + today + ".json")) {
+		try (Reader prevLottoListFile = new FileReader(src + jsonFileName + ".json")) {
+			prevLottoList = (JSONArray) parser.parse(prevLottoListFile);
+		} catch (IOException e) {
+			System.out.println("CSV 파일 읽기 실패");
+			e.printStackTrace();
+
+			// TODO : 오늘날짜로 만든 파일 없으면 그 전날 파일로 읽기
+
+		} catch (ParseException e) {
+			System.out.println("JSON 파싱 실패");
+			e.printStackTrace();
+		}
+
 		return prevLottoList;
+	}
+
+	@SuppressWarnings("unchecked")
+	private ConcurrentHashMap<Integer, Integer> countPrevLotto(JSONArray prevLottoList) {
+		JSONObject lotto = new JSONObject();
+
+		// 번호별 카운트 변수
+		ConcurrentHashMap<Integer, Integer> lottoNumCount = new ConcurrentHashMap<>();
+
+		// 회차별로 쪼개서 번호별 카운트
+		Iterator<JSONObject> iterator = prevLottoList.iterator();
+		while (iterator.hasNext()) {
+			// 로또 한 회차분
+			lotto = iterator.next();
+
+			// TODO : 삭제 (값 확인용)
+			System.out.println(lotto.get("drwtNo1") + " " + lotto.get("drwtNo2") + " " + lotto.get("drwtNo3") + " " + lotto.get("drwtNo4") + " " + lotto.get("drwtNo5") + " " + lotto.get("drwtNo6") + " " + lotto.get("bnusNo"));
+
+			// 번호별 카운트
+			int drwtNo;
+			for (int i = 1; i < 7; i++) {
+				drwtNo = (int) (long) lotto.get("drwtNo" + i);
+
+				if (lottoNumCount.get(drwtNo) == null) {
+					lottoNumCount.put(drwtNo, 1);
+				} else {
+					lottoNumCount.put(drwtNo, lottoNumCount.get(drwtNo) + 1);
+				}
+			}
+
+			// TODO : (옵션) 보너스 번호 카운트
+//			drwtNo = (int) (long) lotto.get("bnusNo");
+//			if (lottoNumCount.get(drwtNo) == null) {
+//				lottoNumCount.put(drwtNo, 1);
+//			} else {
+//				lottoNumCount.put(drwtNo, lottoNumCount.get(drwtNo) + 1);
+//			}
+		}
+
+		return lottoNumCount;
+	}
+
+	private void recommandLottoNum(List<Entry<Integer, Integer>> topNLottoNum) {
+		// TODO : 로또번호 추천
+
 	}
 
 
 
-	// 복사 필요시 사용
+
+
+
+
+
+
+
+	// TODO : 삭제 (복사 필요시 사용)
+	// private ArrayList<LottoVO> dd() {
+	// // 지난회차 로또 정보 담을 리스트
+	// ArrayList<LottoVO> prevLottoList = new ArrayList<LottoVO>();
+	//
+	// // 회차별로 객체에 담기
+	// LottoVO lotto = new LottoVO();
+	// lotto.setReturnValue((String) obj.get("returnValue"));
+	// lotto.setDrwNo((Long) obj.get("drwNo"));
+	// lotto.setDrwtNo1((Long) obj.get("drwtNo1"));
+	// lotto.setDrwtNo2((Long) obj.get("drwtNo2"));
+	// lotto.setDrwtNo3((Long) obj.get("drwtNo3"));
+	// lotto.setDrwtNo4((Long) obj.get("drwtNo4"));
+	// lotto.setDrwtNo5((Long) obj.get("drwtNo5"));
+	// lotto.setDrwtNo6((Long) obj.get("drwtNo6"));
+	// lotto.setBnusNo((Long) obj.get("bnusNo"));
+	// lotto.setFirstWinamnt((Long) obj.get("firstWinamnt"));
+	// lotto.setFirstPrzwnerCo((Long) obj.get("firstPrzwnerCo"));
+	// lotto.setFirstAccumamnt((Long) obj.get("firstAccumamnt"));
+	// lotto.setTotSellamnt((Long) obj.get("totSellamnt"));
+	// lotto.setDrwNoDate((String) obj.get("drwNoDate"));
+	// prevLottoList.add(lotto);
+	// }
+
+	// TODO : 삭제 (복사 필요시 사용)
 	// returnValue
 	// drwNo
 	// drwtNo1
--- src/main/java/com/lotty/first/vo/LottoVO.java
+++ src/main/java/com/lotty/first/vo/LottoVO.java
@@ -2,132 +2,132 @@ package com.lotty.first.vo;
 
 public class LottoVO {
 	private String returnValue;		// 실행결과
-	private Long drwNo;			// 회차
-	private Long drwtNo1;			// 당첨번호1
-	private Long drwtNo2;			// 당첨번호2
-	private Long drwtNo3;			// 당첨번호3
-	private Long drwtNo4;			// 당첨번호4
-	private Long drwtNo5;			// 당첨번호5
-	private Long drwtNo6;			// 당첨번호6
-	private Long bnusNo;			// 보너스번호
-	private Long firstWinamnt;	// 1등 당첨금
-	private Long firstPrzwnerCo;	// 1등 당첨 인원
-	private Long firstAccumamnt;	// 1등 당첨금 총액
-	private Long totSellamnt;		// 누적금
+	private long drwNo;				// 회차
+	private long drwtNo1;			// 당첨번호1
+	private long drwtNo2;			// 당첨번호2
+	private long drwtNo3;			// 당첨번호3
+	private long drwtNo4;			// 당첨번호4
+	private long drwtNo5;			// 당첨번호5
+	private long drwtNo6;			// 당첨번호6
+	private long bnusNo;			// 보너스번호
+	private long firstWinamnt;		// 1등 당첨금
+	private long firstPrzwnerCo;	// 1등 당첨 인원
+	private long firstAccumamnt;	// 1등 당첨금 총액
+	private long totSellamnt;		// 누적금
 	private String drwNoDate;		// 당첨일
-
+	
 	public String getReturnValue() {
 		return returnValue;
 	}
-
+	
 	public void setReturnValue(String returnValue) {
 		this.returnValue = returnValue;
 	}
-
-	public Long getDrwNo() {
+	
+	public long getDrwNo() {
 		return drwNo;
 	}
-
-	public void setDrwNo(Long drwNo) {
+	
+	public void setDrwNo(long drwNo) {
 		this.drwNo = drwNo;
 	}
-
-	public Long getDrwtNo1() {
+	
+	public long getDrwtNo1() {
 		return drwtNo1;
 	}
-
-	public void setDrwtNo1(Long drwtNo1) {
+	
+	public void setDrwtNo1(long drwtNo1) {
 		this.drwtNo1 = drwtNo1;
 	}
-
-	public Long getDrwtNo2() {
+	
+	public long getDrwtNo2() {
 		return drwtNo2;
 	}
-
-	public void setDrwtNo2(Long drwtNo2) {
+	
+	public void setDrwtNo2(long drwtNo2) {
 		this.drwtNo2 = drwtNo2;
 	}
-
-	public Long getDrwtNo3() {
+	
+	public long getDrwtNo3() {
 		return drwtNo3;
 	}
-
-	public void setDrwtNo3(Long drwtNo3) {
+	
+	public void setDrwtNo3(long drwtNo3) {
 		this.drwtNo3 = drwtNo3;
 	}
-
-	public Long getDrwtNo4() {
+	
+	public long getDrwtNo4() {
 		return drwtNo4;
 	}
-
-	public void setDrwtNo4(Long drwtNo4) {
+	
+	public void setDrwtNo4(long drwtNo4) {
 		this.drwtNo4 = drwtNo4;
 	}
-
-	public Long getDrwtNo5() {
+	
+	public long getDrwtNo5() {
 		return drwtNo5;
 	}
-
-	public void setDrwtNo5(Long drwtNo5) {
+	
+	public void setDrwtNo5(long drwtNo5) {
 		this.drwtNo5 = drwtNo5;
 	}
-
-	public Long getDrwtNo6() {
+	
+	public long getDrwtNo6() {
 		return drwtNo6;
 	}
-
-	public void setDrwtNo6(Long drwtNo6) {
+	
+	public void setDrwtNo6(long drwtNo6) {
 		this.drwtNo6 = drwtNo6;
 	}
-
-	public Long getBnusNo() {
+	
+	public long getBnusNo() {
 		return bnusNo;
 	}
-
-	public void setBnusNo(Long bnusNo) {
+	
+	public void setBnusNo(long bnusNo) {
 		this.bnusNo = bnusNo;
 	}
-
-	public Long getFirstWinamnt() {
+	
+	public long getFirstWinamnt() {
 		return firstWinamnt;
 	}
-
-	public void setFirstWinamnt(Long firstWinamnt) {
+	
+	public void setFirstWinamnt(long firstWinamnt) {
 		this.firstWinamnt = firstWinamnt;
 	}
-
-	public Long getFirstPrzwnerCo() {
+	
+	public long getFirstPrzwnerCo() {
 		return firstPrzwnerCo;
 	}
-
-	public void setFirstPrzwnerCo(Long firstPrzwnerCo) {
+	
+	public void setFirstPrzwnerCo(long firstPrzwnerCo) {
 		this.firstPrzwnerCo = firstPrzwnerCo;
 	}
-
-	public Long getFirstAccumamnt() {
+	
+	public long getFirstAccumamnt() {
 		return firstAccumamnt;
 	}
-
-	public void setFirstAccumamnt(Long firstAccumamnt) {
+	
+	public void setFirstAccumamnt(long firstAccumamnt) {
 		this.firstAccumamnt = firstAccumamnt;
 	}
-
-	public Long getTotSellamnt() {
+	
+	public long getTotSellamnt() {
 		return totSellamnt;
 	}
-
-	public void setTotSellamnt(Long totSellamnt) {
+	
+	public void setTotSellamnt(long totSellamnt) {
 		this.totSellamnt = totSellamnt;
 	}
-
+	
 	public String getDrwNoDate() {
 		return drwNoDate;
 	}
-
+	
 	public void setDrwNoDate(String drwNoDate) {
 		this.drwNoDate = drwNoDate;
 	}
-
+	
 	@Override
 	public String toString() {
 		return "LottoVO [returnValue=" + returnValue + ", drwNo=" + drwNo + ", drwtNo1=" + drwtNo1 + ", drwtNo2=" + drwtNo2 + ", drwtNo3=" + drwtNo3 + ", drwtNo4=" + drwtNo4 + ", drwtNo5=" + drwtNo5 + ", drwtNo6=" + drwtNo6 + ", bnusNo=" + bnusNo + ", firstWinamnt=" + firstWinamnt + ", firstPrzwnerCo=" + firstPrzwnerCo + ", firstAccumamnt=" + firstAccumamnt + ", totSellamnt=" + totSellamnt + ", drwNoDate=" + drwNoDate + "]";
